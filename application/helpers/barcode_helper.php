ef044106defc4cb7cb44bbd043e1646021699c23
Gian Akbar <akbarigi20@gmail.com>
2024-09-30 20:30:36 +0700

    Update rack

--- application/helpers/barcode_helper.php
+++ application/helpers/barcode_helper.php
@@ -13,3 +13,21 @@ if (!function_exists('generate_barcode')) {
 		return $barcodeHtml;
 	}
 }
+
+function getGroupedItemsBySloc($sloc)
+{
+	$CI = &get_instance();
+
+	$CI->db->select('r.sloc, b.sku, ba.batchnumber, SUM(bi.qty) as total_quantity');
+	$CI->db->from('rack r');
+	$CI->db->join('rack_items ri', 'r.id_rack = ri.id_rack', 'inner');
+	$CI->db->join('barang b', 'ri.id_barang = b.id_barang', 'inner');
+	$CI->db->join('batch ba', 'ri.id_batch = ba.id_batch', 'inner');
+	$CI->db->join('batchitem bi', 'b.id_barang = bi.id_barang', 'inner');
+	$CI->db->where('r.sloc', $sloc);
+	$CI->db->group_by(['r.sloc', 'b.sku', 'ba.batchnumber']);
+
+	$query = $CI->db->get();
+
+	return $query->result_array();
+}
