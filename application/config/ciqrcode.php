0d9e0176fb4a665c665d3aaf9cedef505654e621
Gian Akbar <akbarigi20@gmail.com>
2024-10-01 00:38:46 +0700

    update lib qr

---
+++ application/config/ciqrcode.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['cacheable']    = true;
+$config['cachedir']     = './application/cache/';
+$config['errorlog']     = './application/logs/';
+$config['quality']      = true;
+$config['size']         = 1024;
+$config['black']        = [224, 255, 255];
+$config['white']        = [70, 130, 180];
