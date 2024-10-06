ff724cf1acc66a07d3b2b12a799e9fce4fa60de7
normanardian <normanardian24@gmail.com>
2024-10-07 00:54:16 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,9 +24,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = "http://wms.test/";
-$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
-$newurl = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
-$config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 // $config['base_url'] = 'https://b555-103-237-140-137.ngrok-free.app/wms';
 
 /*
