552de6f8a5c4c0e4a3af51379cfed033a2fbe641
normanardian <normanardian24@gmail.com>
2024-10-10 11:10:58 +0700

    Update ReceivingInbound_model.php

--- application/models/ReceivingInbound_model.php
+++ application/models/ReceivingInbound_model.php
@@ -45,7 +45,7 @@ class ReceivingInbound_model extends CI_Model
 		$this->db->from('picklist');
 		$this->db->join('datapicklist', 'datapicklist.id_picklist = picklist.id_picklist', 'left');
 		$this->db->where('picklist.uuid', $uuid);
-		$this->db->order_by('datapicklist.id_datapicklist', 'DESC');
+		$this->db->order_by('picklist.id_picklist', 'DESC');
 		return $this->db->get()->row();
 	}
 
